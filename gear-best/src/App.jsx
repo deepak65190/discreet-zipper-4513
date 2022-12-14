31af8ca6b09b80c8d2225a4ceb3f2f0af45ea40d
jkumari25 <jagriti.sj15@gmail.com>
2022-12-14 22:02:28 +0530

    added navbar and category

--- gear-best/src/App.jsx
+++ gear-best/src/App.jsx
@@ -1,12 +1,16 @@
-
 import './App.css'
-import Footer from "./footer/Footer"
+import Footer from "./footer/Footer";
+import Navbar from "./Navbar/Navbar";
+import Category from "./Category/Category";
+
+
 function App() {
   
 
   return (
     <div className="App">
-    
+      <Navbar />
+      <Category />
       <Footer/>
    
     </div>
--- gear-best/src/main.jsx
+++ gear-best/src/main.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+import './index.css';
+import  {ChakraProvider} from "@chakra-ui/react";
+import { BrowserRouter } from 'react-router-dom';
+import { extendTheme } from '@chakra-ui/react'
+
+const breakpoints = {
+  base:'0px',
+  md: '600px',
+  lg: '900px',
+
+}
+const theme = extendTheme({ breakpoints })
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+   <BrowserRouter>
+    <ChakraProvider theme={theme}>
+     <App />
+    </ChakraProvider>
+   </BrowserRouter>
+  </React.StrictMode>
 )
