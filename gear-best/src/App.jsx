5c81d9c49e97cb0b94090601f9f5055dec346f81
jkumari25 <jagriti.sj15@gmail.com>
2022-12-15 16:21:52 +0530

    updated

--- gear-best/src/App.jsx
+++ gear-best/src/App.jsx
@@ -1,13 +1,15 @@
 import './App.css'
-// import Footer from "./footer/Footer";
-// import Navbar from "./Navbar/Navbar";
-// import Category from "./Category/Category";
+import Footer from "./footer/Footer";
+import Navbar from "./Navbar/Navbar";
+import Category from "./Category/Category";
 
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar />
+      <Category />
+      <Footer />
     </div>
   )
 }
