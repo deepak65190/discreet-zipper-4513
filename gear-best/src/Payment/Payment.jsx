965be4c7d2547535f4e8e6ad536bed02edc5d39f
sanju682295 <sanju682295@gmail.com>
2022-12-18 22:50:20 +0530

    updated

--- gear-best/src/Payment/Payment.jsx
+++ gear-best/src/Payment/Payment.jsx
@@ -2,12 +2,40 @@ import styles from "./Payment.module.css";
 import { useState,useEffect } from "react";
 import { useSearchParams } from "react-router-dom";
 import { Link } from "react-router-dom";
+let initialDetails={
+  name:'',
+  email:'',
+  address:'',
+  city:'',
+  district:'',
+  phone:'',
+  mode:''
+}
 const Payment = () => {
+  const [data,setData]=useState(initialDetails)
+  const [fname,setFName]=useState('')
+  const [lname,setLName]=useState('')
+  const [email,setEmail]=useState('')
+  const [address,setAdd]=useState('')
+  const [city,setCity]=useState('')
+  const [district,setDist]=useState('')
+  const [phone,setPhone]=useState('')
+  const [state,setState]=useState('')
   const [searchParams,setSearchParams]=useSearchParams();
   const initialMode=searchParams.getAll('mode');
   const [mode,setMode]=useState(initialMode[0]||'');
  const  handleMode=(e)=>{
 setMode(e.target.value)
+  }
+
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    if(fname==='' || lname==='' || email==='' || address==='' || city==='' || district==='' || phone==='' || state===''){
+      alert('Fill All Credintials')
+    }else{
+      setData({...data,name:`${fname} ${lname}`,email:email,address:address,city:city,district:district,phone:phone,state:state,mode:mode})
+      localStorage.setItem('userDetails',JSON.stringify(data))
+    }
   }
 
 useEffect(()=>{
@@ -29,25 +57,25 @@ setSearchParams(params)
       <div>
         <div className={styles.form}>
           <h1>Enter Your Details</h1>
-          <form action="">
+          <form action="" onSubmit={handleSubmit} >
             <label htmlFor="">Name : </label>{" "}
-            <input type="text" placeholder="First Name" />
-            <input type="text" placeholder="Last Name" />
+            <input type="text" placeholder="First Name" onChange={(e)=>setFName(e.target.value)}/>
+            <input type="text" placeholder="Last Name" onChange={(e)=>setLName(e.target.value)}/>
             <br />
             <label htmlFor="">Email Address : </label>
-            <input type="email" placeholder="Email Address" />
+            <input type="email" placeholder="Email Address" onChange={(e)=>setEmail(e.target.value)}/>
             <br />
             <label htmlFor="">Phone Number : </label>
-            <input type="number" placeholder="Phone Number" />
+            <input type="number" placeholder="Phone Number" onChange={(e)=>setPhone(e.target.value)}/>
             <br />
             <label htmlFor="">City : </label>
-            <input type="text" placeholder="Enter City Name" />
+            <input type="text" placeholder="Enter City Name" onChange={(e)=>setCity(e.target.value)} />
             <br />
             <label htmlFor="">District : </label>
-            <input type="text" placeholder="Enter District Name" />
+            <input type="text" placeholder="Enter District Name" onChange={(e)=>setDist(e.target.value)}/>
             <br />
             <label htmlFor="">State : </label>
-            <input type="text" placeholder="Enter State Name" />
+            <input type="text" placeholder="Enter State Name" onChange={(e)=>setState(e.target.value)}/>
             <br />
             <div>
               <div className={styles.payment}>
@@ -82,7 +110,7 @@ setSearchParams(params)
                 />
                 <label>Net Banking</label>
               </div>
-              <button className={styles.btn}>Continue</button>
+              <button className={styles.btn} type="submit">Continue</button>
             </div>
           </form>
         </div>
